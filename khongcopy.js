d23c3261ba0999a915de58fc07a4d0ae4a311c12
khuongbuivu <seokhuong@gmail.com>
2016-02-27 11:46:29 +0700

    Update khongcopy.js

--- khongcopy.js
+++ khongcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>Vào dây copy nhé : <a href='http://dieukhacninhlong.com'>http://dieukhacninhlong.com"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>Vao Link Copy Nhe : <a href='http://dieukhacninhlong.com'>http://dieukhacninhlong.com"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
